12b6a6326b75f5b297bcfabb9db418f973704487
Iamneo <techadmin@iamneo.ai>
2023-08-08 06:44:27 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,4 +1,3 @@
-app.component.ts
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,15 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularapp';
-  opt1:number=0;
-  opt2:number=0;
-  amt:number=0;
-  res:number=0;
-  result:string='';
-  exchange(a:number,b:number,amt:number){
-    if(a!=null && b!=null){
-      this.result=Math.round(b/a*amt).toFixed(2);
+  opt1: number = 0;
+  opt2: number = 0;
+  amt: number = 0;
+  res: string = '';
+  result: string = '';
+
+  exchange(a: number, b: number, amt: number): string {
+    if (a !== 0 && b !== 0) {
+      this.result=Math.round(amt/(a/b)).toFixed(2);
       return this.result;
     }
-  }
+    return '';
+  }
 }
